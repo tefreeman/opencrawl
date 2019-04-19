6d1e3b7ce77e27ccb1c417516bf4d81ed8bf5407
Trevor Freeman <tefreeman@crimson.ua.edu>
2019-04-18 20:09:00 -0500

    fixed allrecipe nutrition and info parsing errors

--- openextractor/allrecipe.py
+++ openextractor/allrecipe.py
@@ -144,19 +144,56 @@ def get_nutrition_info(parser: BeautifulSoup) -> dict:
     if nutri_selector is not None:
         nutri_elements: List[Tag] = nutri_selector.select('span')
         for ele in nutri_elements:
-            nutrient_name =  ele.get('itemprop')
-            if nutrient_name:
+            nutrient_name = ele.get('itemprop')
+            if nutrient_name is not None:
                 nutrition_dict[nutrient_name] = ele.text.strip()
+    else:
+        nutri_selector: Tag = parser.select_one('div.recipe-nutrition__form')
+
+        if nutri_selector is not None:
+            nutrient_names = nutri_selector.select('li.nutrientLine__item')
+            nutrient_vals = nutri_selector.select('li.nutrientLine__item--amount')
+
+            length = len(nutrient_names)
+            if len(nutrient_vals) < length:
+                length = len(nutrient_vals)
+
+            for i in range(0, length):
+                try:
+                    nutrition_dict[nutrient_names[i].text.strip().replace('.', '')] = nutrient_vals[i].text.strip()
+                except:
+                    pass
+
     return nutrition_dict
 
 
 def get_time_info(parser: BeautifulSoup) -> List[str]:
-    r_list: List[str] = []
+    r_list: List[Union[str, dict]] = []
     prep_list: List[Tag] = parser.select('li.prepTime__item')
     if prep_list is not None:
         for prep in prep_list:
             prep_text =  prep.get('aria-label')
             if prep_text:
                 r_list.append(prep_text)
-    return r_list
+    if len(r_list) > 0:
+        return r_list
+    try:
+        yield_tag = parser.find(itemprop='recipeYield')
+        if yield_tag is not None:
+            r_list.append({'yield': yield_tag.get('content').strip()})
 
+        ready_time = parser.select_one('span.ready-in-time')
+        if ready_time is not None:
+            r_list.append({'total': ready_time.text.strip()})
+
+        prep_time = parser.find('time', itemprop='prepTime')
+        if prep_time is not None:
+            r_list.append({'prep': prep_time.text.strip()})
+
+        cook_time = parser.find('time', itemprop='cookTime')
+        if cook_time is not None:
+            r_list.append({'cook': cook_time.text.strip()})
+    except:
+        return r_list
+
+    return r_list
